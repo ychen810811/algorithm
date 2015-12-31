4f76bfb25da73f26371a6b53f018c29d5626f9e5
ychen810811 <chen.elliott@gmail.com>
2015-12-31 12:42:03 +0800

    Initial: implemented insertion-sort algorithm

---
+++ main.py
@@ -0,0 +1,12 @@
+import sort
+
+
+def main():
+    # Insertion-sort
+    a = [5, 2, 4, 6, 1, 3]
+    print(a)
+    sort.insertion_sort(a)
+    print(a)
+
+if __name__ == '__main__':
+    main()
---
+++ sort.py
@@ -0,0 +1,8 @@
+def insertion_sort(a):
+    for j in range(1, len(a)):
+        key = a[j]
+        i = j - 1
+        while i >= 0 and a[i] > key:
+            a[i + 1] = a[i]
+            i -= 1
+        a[i + 1] = key
