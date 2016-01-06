8d3b15489f24e799d8555f03b66e2341a6cd5cb5
ychen810811 <chen.elliott@gmail.com>
2016-01-06 17:47:04 +0800

    Try to test sort with a large array - it ends with exception of "too large int to convert to C sszie_t"
    It seems Python supports large integers in arithmetic, but not in
    operations like range(n)

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import sort
+import random
+
+
+def Fermat(n):
+    fermat = []
+    for i in range(n):
+        fermat.append(2 ** (2 ** i) + 1)
+    return fermat
 
 
 def main():
     # Insertion-sort
-    a = [5, 2, 4, 6, 1, 3]
+    # a = list(range(2 ** 31))
+    a = []
+    i = 0
+    while i < 2 ** 10:
+        a.append(random.randrange(1, 2 ** 10))
+        i += 1
     print(a)
+    random.shuffle(a)
     sort.insertion_sort(a)
     print(a)
 
