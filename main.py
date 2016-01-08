4af05b6abd2a466581518c58aae086146437f78f
ychen810811 <chen.elliott@gmail.com>
2016-01-08 17:14:24 +0800

    Implemented merge() routine for MERGE SORT algorithm

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def Fermat(n):
     return fermat
 
 
-def main():
+def insertion_sort_test():
     a = []
     i = 0
     while i < 2 ** 10:
@@ -19,6 +19,29 @@ def main():
     random.shuffle(a)
     sort.insertion_sort(a)
     print(a)
+
+
+def merge_sort_test():
+    A1 = []
+    A2 = []
+    for i in range(20):
+        A1.append(random.randrange(1, 2 ** 5))
+    for i in range(10):
+        A2.append(random.randrange(1, 2 ** 5))
+    sort.insertion_sort(A1)
+    sort.insertion_sort(A2)
+    print(A1)
+    print(A2)
+    A = A1 + A2
+    print(A)
+    sort.merge(A, 0, 19, 29)
+    print(A)
+
+
+def main():
+    # insertion_sort_test()
+    merge_sort_test()
+
 
 if __name__ == '__main__':
     main()
--- sort.py
+++ sort.py
@@ -1,3 +1,4 @@
+# INSERTION SORT algorithm implementation
 def insertion_sort(a):
     for j in range(1, len(a)):
         key = a[j]
@@ -6,3 +7,29 @@ def insertion_sort(a):
             a[i + 1] = a[i]
             i -= 1
         a[i + 1] = key
+
+
+# Merge function for MERGE SORT algorithm implementation
+def merge(A, p, q, r):
+    L = A[p:q + 1]
+    R = A[q + 1:r + 1]
+    L.append(None)
+    R.append(None)
+    j = k = 0
+    for i in range(p, r + 1):
+        try:
+            if L[j] <= R[k]:
+                A[i] = L[j]
+                j += 1
+            else:
+                A[i] = R[k]
+                k += 1
+        except TypeError as e:
+            if L[j] is None:
+                A[i:r + 1] = R[k:-1]
+                break
+            elif R[k] is None:
+                A[i:r + 1] = L[j:-1]
+                break
+            else:
+                raise e
