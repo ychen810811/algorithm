c48205c0c7cf3c6270eef75bc924999df5071582
Elliott Chen <chen.elliott@gmail.com>
2016-01-22 21:47:23 +0800

    Resolved divergence from the remote master

--- max_subarray.py
+++ max_subarray.py
@@ -5,7 +5,7 @@
 def max_cross_subarray(A, low, mid, high):
     left_sum = None
     tmp = 0
-    for i in range(low, mid + 1):
+    for i in range(mid, low - 1, -1):
         tmp += A[i]
         try:
             if tmp > left_sum:
@@ -14,6 +14,7 @@ def max_cross_subarray(A, low, mid, high):
         except TypeError:
             if left_sum is None:
                 left_sum = tmp
+                left_index_max = i
             else:
                 raise
 
@@ -28,23 +29,25 @@ def max_cross_subarray(A, low, mid, high):
         except TypeError:
             if right_sum is None:
                 right_sum = tmp
+                right_index_max = j
             else:
                 raise
     return (left_index_max, right_index_max, left_sum + right_sum)
 
 
 # 4.1: for sort method: key function
-def tupple_element_2(t):
+def _tupple_element_2(t):
     return t[2]
+
 
 # 4.1: MAX SUBARRAY algorithm
 def max_subarray(A, low, high):
     if low == high:
         return (low, high, A[low])
 
-    t_left = max_subarray(A, low, (low + high) // 2)
-    t_right = max_subarray(A, (low + high) // 2 + 1, high)
-    t_cross = max_cross_subarray(A, low, (low + high) // 2, high)
-    t_list = [t_left, t_right, t_cross]
-    t_list.sort(key=tupple_element_2, reverse=True)
-    return t_list[0]
+    candidate_left = max_subarray(A, low, (low + high) // 2)
+    candidate_right = max_subarray(A, (low + high) // 2 + 1, high)
+    candidate_cross = max_cross_subarray(A, low, (low + high) // 2, high)
+    candidate_list = [candidate_left, candidate_right, candidate_cross]
+    candidate_list.sort(key=_tupple_element_2, reverse=True)
+    return candidate_list[0]
