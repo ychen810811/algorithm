1071ac5adab56fd45549586e9da8615cded2839e
Elliott Chen <chen.elliott@gmail.com>
2016-02-01 22:59:00 +0800

    No way to merge sub-matrices to a single one...

--- matrix.py
+++ matrix.py
@@ -14,6 +14,16 @@ def square_matrix_multiply(A, B):
     return C
 
 
+# Internal: matrice addition
+def _add(A, B):
+    n = len(A)
+    C = [[None] * n] * n
+    for i in range(n):
+        for j in range(n):
+            C[i][j] = A[i][j] + B[i][j]
+    return C
+
+
 # 4.2: SQUARE MATRIX MULTIPLICATION - Strassen algorithm
 def square_matrix_multiply_strassen(A, B):
     "4.2: Strassen algorithm of SQUARE MATRIX MULTIPLICATION"
@@ -30,4 +40,7 @@ def square_matrix_multiply_strassen(A, B):
     B_1_2 = [x[n // 2:] for x in B[:n // 2]]
     B_2_1 = [x[:n // 2] for x in B[n // 2:]]
     B_2_2 = [x[n // 2:] for x in B[n // 2:]]
-    C_1_1 =
+    C_1_1 = _add(square_matrix_multiply_strassen(A_1_1, B_1_1), square_matrix_multiply_strassen(A_1_2, B_2_1))
+    C_1_2 = _add(square_matrix_multiply_strassen(A_1_1, B_1_2), square_matrix_multiply_strassen(A_1_2, B_2_2))
+    C_2_1 = _add(square_matrix_multiply_strassen(A_2_1, B_1_1), square_matrix_multiply_strassen(A_2_2, B_2_1))
+    C_2_2 = _add(square_matrix_multiply_strassen(A_2_1, B_1_2), square_matrix_multiply_strassen(B_2_2, B_2_2))
