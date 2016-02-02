c9875ba3b0a100ca773f881a003becea22bafbd4
Y. Chen <chen.elliott@gmail.com>
2016-02-02 13:15:20 +0800

    Fixed bug in matrice multiplication calculation

--- matrix.py
+++ matrix.py
@@ -43,7 +43,7 @@ def square_matrix_multiply_strassen(A, B):
     C_1_1 = _add(square_matrix_multiply_strassen(A_1_1, B_1_1), square_matrix_multiply_strassen(A_1_2, B_2_1))
     C_1_2 = _add(square_matrix_multiply_strassen(A_1_1, B_1_2), square_matrix_multiply_strassen(A_1_2, B_2_2))
     C_2_1 = _add(square_matrix_multiply_strassen(A_2_1, B_1_1), square_matrix_multiply_strassen(A_2_2, B_2_1))
-    C_2_2 = _add(square_matrix_multiply_strassen(A_2_1, B_1_2), square_matrix_multiply_strassen(B_2_2, B_2_2))
+    C_2_2 = _add(square_matrix_multiply_strassen(A_2_1, B_1_2), square_matrix_multiply_strassen(A_2_2, B_2_2))
     for i in range(n // 2):
         C[i][:n // 2] = C_1_1[i]
         C[i][n // 2:] = C_1_2[i]
