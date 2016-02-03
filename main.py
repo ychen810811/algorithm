6a95a899494cca88e349fd7acf3ec374bf9b781d
Y. Chen <chen.elliott@gmail.com>
2016-02-04 00:26:29 +0800

    Just realized that it was not "Strassen" method yet, renamed all the relative names

--- main.py
+++ main.py
@@ -47,20 +47,20 @@ def test_matrix_multiply():
     print(C)
 
 
-def test_matrix_multiply_strassen():
+def test_matrix_multiply_recursive():
     A = [[1, 3, 2, 1], [4, 6, 1, 3], [5, 8, 9, 4], [1, 3, 3, 6]]
     B = [[1, 5, 6, 9], [2, 4, 1, 6], [3, 3, 7, 5], [4, 1, 8, 4]]
-    C = matrix.square_matrix_multiply_strassen(A, B)
+    C = matrix.square_matrix_multiply_recursive(A, B)
     print(C)
 
 
-def test_matrix_multiply_in_place():
+def test_matrix_multiply_recursive_in_place():
     C = [[0 for j in range(4)] for i in range(4)]
     A = [[1, 3, 2, 1], [4, 6, 1, 3], [5, 8, 9, 4], [1, 3, 3, 6]]
     B = [[1, 5, 6, 9], [2, 4, 1, 6], [3, 3, 7, 5], [4, 1, 8, 4]]
     size = len(C)
     idx_range = [list(range(size)), list(range(size))]
-    matrix.multiply_in_place(C, A, B, idx_range, idx_range, idx_range, size)
+    matrix.multiply_recursive_in_place(C, A, B, idx_range, idx_range, idx_range, size)
     print(C)
 
 
@@ -71,8 +71,8 @@ def main():
     # max_subarray_test()
     # max_subarray_time_linear_test()
     test_matrix_multiply()
-    test_matrix_multiply_strassen()
-    test_matrix_multiply_in_place()
+    test_matrix_multiply_recursive()
+    test_matrix_multiply_recursive_in_place()
 
 
 if __name__ == '__main__':
--- matrix.py
+++ matrix.py
@@ -25,9 +25,9 @@ def _add(A, B):
     return C
 
 
-# 4.2: SQUARE MATRIX MULTIPLICATION - Strassen algorithm
-def square_matrix_multiply_strassen(A, B):
-    "4.2: Strassen algorithm of SQUARE MATRIX MULTIPLICATION"
+# 4.2: SQUARE MATRIX MULTIPLICATION - Simple divide-and-conquer
+def square_matrix_multiply_recursive(A, B):
+    "4.2: Simple divide-and-conquer algorithm of SQUARE MATRIX MULTIPLICATION"
     n = len(A)
     if n == 1:
         return [[A[0][0] * B[0][0]]]
@@ -41,10 +41,10 @@ def square_matrix_multiply_strassen(A, B):
     B_1_2 = [x[n // 2:] for x in B[:n // 2]]
     B_2_1 = [x[:n // 2] for x in B[n // 2:]]
     B_2_2 = [x[n // 2:] for x in B[n // 2:]]
-    C_1_1 = _add(square_matrix_multiply_strassen(A_1_1, B_1_1), square_matrix_multiply_strassen(A_1_2, B_2_1))
-    C_1_2 = _add(square_matrix_multiply_strassen(A_1_1, B_1_2), square_matrix_multiply_strassen(A_1_2, B_2_2))
-    C_2_1 = _add(square_matrix_multiply_strassen(A_2_1, B_1_1), square_matrix_multiply_strassen(A_2_2, B_2_1))
-    C_2_2 = _add(square_matrix_multiply_strassen(A_2_1, B_1_2), square_matrix_multiply_strassen(A_2_2, B_2_2))
+    C_1_1 = _add(square_matrix_multiply_recursive(A_1_1, B_1_1), square_matrix_multiply_recursive(A_1_2, B_2_1))
+    C_1_2 = _add(square_matrix_multiply_recursive(A_1_1, B_1_2), square_matrix_multiply_recursive(A_1_2, B_2_2))
+    C_2_1 = _add(square_matrix_multiply_recursive(A_2_1, B_1_1), square_matrix_multiply_recursive(A_2_2, B_2_1))
+    C_2_2 = _add(square_matrix_multiply_recursive(A_2_1, B_1_2), square_matrix_multiply_recursive(A_2_2, B_2_2))
     for i in range(n // 2):
         C[i][:n // 2] = C_1_1[i]
         C[i][n // 2:] = C_1_2[i]
@@ -53,10 +53,10 @@ def square_matrix_multiply_strassen(A, B):
     return C
 
 
-# 4.2: Strassen algorithm - this version implements in-place calculation to
+# 4.2: Simple divide-and-conquer - this version implements in-place calculation to
 # avoid copying elements to new sub-matrices (resulting to O(n^2) cost)
-def multiply_in_place(C, A, B, C_index_range, A_index_range, B_index_range, size):
-    "4.2: Strassen algorithm of SQUARE MATRIX MULTIPLICATION - inline calculation"
+def multiply_recursive_in_place(C, A, B, C_index_range, A_index_range, B_index_range, size):
+    "4.2: Simple divide-and-conquer algorithm of SQUARE MATRIX MULTIPLICATION - inline calculation"
     if size == 1:
         C[C_index_range[0][0]][C_index_range[1][0]] += A[A_index_range[0][0]][A_index_range[1][0]] * B[B_index_range[0][0]][B_index_range[1][0]]
         return C
@@ -65,36 +65,36 @@ def multiply_in_place(C, A, B, C_index_range, A_index_range, B_index_range, size
     C_sub_range = [C_index_range[0][:size // 2], C_index_range[1][:size // 2]]
     A_sub_range = [A_index_range[0][:size // 2], A_index_range[1][:size // 2]]
     B_sub_range = [B_index_range[0][:size // 2], B_index_range[1][:size // 2]]
-    multiply_in_place(C, A, B, C_sub_range, A_sub_range, B_sub_range, size // 2)
+    multiply_recursive_in_place(C, A, B, C_sub_range, A_sub_range, B_sub_range, size // 2)
     A_sub_range = [A_index_range[0][:size // 2], A_index_range[1][size // 2:]]
     B_sub_range = [B_index_range[0][size // 2:], B_index_range[1][:size // 2]]
-    multiply_in_place(C, A, B, C_sub_range, A_sub_range, B_sub_range, size // 2)
+    multiply_recursive_in_place(C, A, B, C_sub_range, A_sub_range, B_sub_range, size // 2)
 
     # C12 = A11 * B12 + A12 * B22
     C_sub_range = [C_index_range[0][:size // 2], C_index_range[1][size // 2:]]
     A_sub_range = [A_index_range[0][:size // 2], A_index_range[1][:size // 2]]
     B_sub_range = [B_index_range[0][:size // 2], B_index_range[1][size // 2:]]
-    multiply_in_place(C, A, B, C_sub_range, A_sub_range, B_sub_range, size // 2)
+    multiply_recursive_in_place(C, A, B, C_sub_range, A_sub_range, B_sub_range, size // 2)
     A_sub_range = [A_index_range[0][:size // 2], A_index_range[1][size // 2:]]
     B_sub_range = [B_index_range[0][size // 2:], B_index_range[1][size // 2:]]
-    multiply_in_place(C, A, B, C_sub_range, A_sub_range, B_sub_range, size // 2)
+    multiply_recursive_in_place(C, A, B, C_sub_range, A_sub_range, B_sub_range, size // 2)
 
     # C21 = A21 * B11 + A22 * B21
     C_sub_range = [C_index_range[0][size // 2:], C_index_range[1][:size // 2]]
     A_sub_range = [A_index_range[0][size // 2:], A_index_range[1][:size // 2]]
     B_sub_range = [B_index_range[0][:size // 2], B_index_range[1][:size // 2]]
-    multiply_in_place(C, A, B, C_sub_range, A_sub_range, B_sub_range, size // 2)
+    multiply_recursive_in_place(C, A, B, C_sub_range, A_sub_range, B_sub_range, size // 2)
     A_sub_range = [A_index_range[0][size // 2:], A_index_range[1][size // 2:]]
     B_sub_range = [B_index_range[0][size // 2:], B_index_range[1][:size // 2]]
-    multiply_in_place(C, A, B, C_sub_range, A_sub_range, B_sub_range, size // 2)
+    multiply_recursive_in_place(C, A, B, C_sub_range, A_sub_range, B_sub_range, size // 2)
 
     # C22 = A21 * B12 + A22 * B22
     C_sub_range = [C_index_range[0][size // 2:], C_index_range[1][size // 2:]]
     A_sub_range = [A_index_range[0][size // 2:], A_index_range[1][:size // 2]]
     B_sub_range = [B_index_range[0][:size // 2], B_index_range[1][size // 2:]]
-    multiply_in_place(C, A, B, C_sub_range, A_sub_range, B_sub_range, size // 2)
+    multiply_recursive_in_place(C, A, B, C_sub_range, A_sub_range, B_sub_range, size // 2)
     A_sub_range = [A_index_range[0][size // 2:], A_index_range[1][size // 2:]]
     B_sub_range = [B_index_range[0][size // 2:], B_index_range[1][size // 2:]]
-    multiply_in_place(C, A, B, C_sub_range, A_sub_range, B_sub_range, size // 2)
+    multiply_recursive_in_place(C, A, B, C_sub_range, A_sub_range, B_sub_range, size // 2)
 
     return C
